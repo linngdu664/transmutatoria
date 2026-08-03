d5e1270d6b24e2f50af855440aebb887485023b7
zx1316 <1315862279@qq.com>
2026-08-03 13:15:13 +0800

    女仆炼金时使用空闲音效

--- src/main/java/com/linngdu664/transmutatoria/compat/touhoulittlemaid/AlchemyMaidTask.java
+++ src/main/java/com/linngdu664/transmutatoria/compat/touhoulittlemaid/AlchemyMaidTask.java
@@ -2,6 +2,8 @@ package com.linngdu664.transmutatoria.compat.touhoulittlemaid;
 
 import com.github.tartaricacid.touhoulittlemaid.api.task.IMaidTask;
 import com.github.tartaricacid.touhoulittlemaid.entity.passive.EntityMaid;
+import com.github.tartaricacid.touhoulittlemaid.init.InitSounds;
+import com.github.tartaricacid.touhoulittlemaid.util.SoundUtil;
 import com.google.common.collect.Lists;
 import com.linngdu664.transmutatoria.ArsTransmutatoria;
 import com.linngdu664.transmutatoria.init.InitItems;
@@ -11,7 +13,6 @@ import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.entity.ai.behavior.BehaviorControl;
 import net.minecraft.world.item.ItemStack;
 
-import javax.annotation.Nullable;
 import java.util.List;
 
 public final class AlchemyMaidTask implements IMaidTask {
@@ -28,8 +29,8 @@ public final class AlchemyMaidTask implements IMaidTask {
     }
 
     @Override
-    public @Nullable SoundEvent getAmbientSound(EntityMaid maid) {
-        return null;
+    public SoundEvent getAmbientSound(EntityMaid maid) {
+        return SoundUtil.environmentSound(maid, InitSounds.MAID_IDLE.get(), 0.5f);
     }
 
     @Override
